abe5e85cbc5c60f15b64231f2e76a8af024a87d0
Paul Kurucz <pkurucz@gmail.com>
2019-02-07 12:33:16 -0500

    Initial commit

---
+++ test_projects/i2c_bus_scan/i2c_bus_scan.ino
@@ -0,0 +1,99 @@
+#include <Wire.h>
+
+/*
+ * Ouput when scanning a Nano RX (no associated to a TX):
+ *
+ * Scanning...
+ * I2C device found at address 0x01  !
+ * I2C device found at address 0x02  !
+ * I2C device found at address 0x03  !
+ * I2C device found at address 0x04  !
+ * I2C device found at address 0x05  !
+ * I2C device found at address 0x06  !
+ * I2C device found at address 0x07  !
+ * I2C device found at address 0x76  !
+ * I2C device found at address 0x7C  !
+ * I2C device found at address 0x7D  !
+ * I2C device found at address 0x7E  !
+ * done
+ * 
+ */
+
+#define LED_ON() digitalWrite(LED_BUILTIN, HIGH)
+#define LED_OFF() digitalWrite(LED_BUILTIN, LOW)
+
+// the setup routine runs once when you press reset:
+void setup() {
+  Serial.begin(115200, SERIAL_8N1);
+
+  /* I2C Init, make sure the internal pull-ups are enabled */
+#if 1
+  pinMode (SDA, INPUT_PULLUP);
+  pinMode (SCL, INPUT_PULLUP);
+  digitalWrite(SDA, 1);
+  digitalWrite(SCL, 1);
+#endif
+  Wire.begin();
+  
+  pinMode(LED_BUILTIN, OUTPUT);
+  LED_OFF();
+
+  while (!Serial);             // Leonardo: wait for serial monitor
+  Serial.println("\nI2C Scanner");
+
+  /* Toggle the led to indicate setup complete */
+  for(int i = 0; i < 5; i++) {
+    LED_ON();
+    delay(700);
+    LED_OFF();
+    delay(300);
+  }
+}
+
+// the loop routine runs over and over again forever:
+void loop() {
+    byte error, address;
+  int nDevices;
+ 
+  Serial.println("Scanning...");
+ 
+  nDevices = 0;
+  for(address = 1; address < 127; address++ )
+  {
+    // The i2c_scanner uses the return value of
+    // the Write.endTransmisstion to see if
+    // a device did acknowledge to the address.
+    Wire.beginTransmission(address);
+    error = Wire.endTransmission();
+ 
+    if (error == 0)
+    {
+      Serial.print("I2C device found at address 0x");
+      if (address<16)
+        Serial.print("0");
+      Serial.print(address,HEX);
+      Serial.println("  !");
+ 
+      nDevices++;
+    }
+    else if (error==4)
+    {
+      Serial.print("Unknown error at address 0x");
+      if (address<16)
+        Serial.print("0");
+      Serial.println(address,HEX);
+    }    
+  }
+  if (nDevices == 0)
+    Serial.println("No I2C devices found\n");
+  else
+    Serial.println("done\n");
+
+    LED_ON();
+    delay(500);
+    LED_OFF();
+    delay(500);
+
+  delay(5000);           // wait 5 seconds for next scan
+}
+
